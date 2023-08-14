ae2c9f1bbccf061028e0382ce025095cb3d97b4f
ritesh <riteshp.softprodigy@gmail.com>
2023-08-14 11:32:27 +0530

    new database set

--- users/models.py
+++ users/models.py
@@ -3,43 +3,28 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
 
 
-
-
-
 class Roles(models.Model):
     name = models.CharField(max_length=100, blank=False)
+
     def __str__(self):
         return self.name
-    
 
 
 class User(AbstractUser):
-    roles = models.ForeignKey(Roles, on_delete=models.PROTECT, null=True ,default = 2 )
-
+    roles = models.ForeignKey(
+        Roles, on_delete=models.PROTECT, null=True, default=2)
 
 
 class UserProfile(models.Model):
-    user=models.OneToOneField(User, on_delete=models.PROTECT ,related_name='userprofile')
-    profession = models.CharField(max_length=255,null=True, blank=True)
-    profilePic = models.ImageField(upload_to='static/profilepic',null=True,blank=True)
-    verification_code = models.CharField(max_length=100 ,null = True)
+    user = models.OneToOneField(
+        User, on_delete=models.PROTECT, related_name='userprofile')
+    profession = models.CharField(max_length=255, null=True, blank=True)
+    profilePic = models.ImageField(
+        upload_to='static/profilepic', null=True, blank=True)
+    verification_code = models.CharField(max_length=100, null=True)
     phone = models.BigIntegerField(null=True, blank=True)
-    about = models.CharField(max_length=555,null=True, blank=True)
-    address= models.CharField(max_length=255,null=True, blank=True)
-    city= models.CharField(max_length=255,null=True, blank=True)
-    state= models.CharField(max_length=255,null=True, blank=True)
-    zip= models.TextField(null=True, blank=True)
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
+    about = models.CharField(max_length=555, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    state = models.CharField(max_length=255, null=True, blank=True)
+    zip = models.TextField(null=True, blank=True)
