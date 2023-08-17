56478a068649b878fa037cac45ef093e2660e3b4
ritesh <riteshp.softprodigy@gmail.com>
2023-08-17 15:50:01 +0530

    dgf

--- recipe/pagination.py
+++ recipe/pagination.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class PagePagination(PageNumberPagination):
-    page_size = 15
+    page_size = 16
     page_query_param = "page"
 
 class PagePaginationAdmin(PageNumberPagination):
